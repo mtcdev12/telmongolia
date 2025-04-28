f65b276c9866d75c3958113be1a071ecbdb25e93
Ganzorig1215 <lhganzorig1215@gmail.com>
2025-04-28 22:01:17 +0800

    ganzo drive share

--- src/app/shareholders/document/page.js
+++ src/app/shareholders/document/page.js
@@ -1,24 +1,24 @@
-// import React from 'react'
-// import Link from "next/link";
-// // import { appList } from "../../../../public/assets/document/2025 хувьцаа last.pdf";
-// function page() {
-//   return (
-//     <div className='p-8'>
-//                 <Link href="/shareholders" >      
-//         <button
-//         className="px-4 py-1 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition mb-5"
-//       >
-//         Буцах
-//       </button></Link>
-//               <iframe
-//                 // src={`http://202.179.23.66:8088/eoffice/uploads/1745840198260.pdf`}
-//                 src='/assets/document/2025 хувьцаа last.pdf'
-//                 width="100%"
-//                 height="700px"
-//                      className='mt-3'
-//               ></iframe>
-//     </div>
-//   )
-// }
+import React from 'react'
+import Link from "next/link";
+// import { appList } from "../../../../public/assets/document/2025 хувьцаа last.pdf";
+function page() {
+    return (
+        <div className='p-8'>
+            {/* <Link href="/shareholders" >
+                <button
+                    className="px-4 py-1 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition mb-5"
+                >
+                    Буцах
+                </button></Link>
+            <iframe
+                // src={`http://202.179.23.66:8088/eoffice/uploads/1745840198260.pdf`}
+                src='/assets/document/2025 хувьцаа last.pdf'
+                width="100%"
+                height="700px"
+                className='mt-3'
+            ></iframe> */}
+        </div>
+    )
+}
 
-// export default page
+export default page
