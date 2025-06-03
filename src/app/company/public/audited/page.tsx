9c104255b3613f6a972d043d77325ff20ff1bb33
Ganzorig1215 <lhganzorig1215@gmail.com>
2025-06-03 09:32:39 +0800

    2025-2024

--- src/app/company/public/audited/page.tsx
+++ src/app/company/public/audited/page.tsx
@@ -3,7 +3,7 @@ import PdfTable from "../../pdfTable";
 
 const breadcrumb = ["Ил тод байдал", "Аудитлагдсан тайлан"];
 const duties = [
-    ['2025 оны санхүүгийн аудитын тайлан', 'iltodbaidal/audited balance 2025.pdf'],
+    ['2024 оны санхүүгийн аудитын тайлан', 'iltodbaidal/audited balance 2025.pdf'],
     ['2023 оны санхүүгийн аудитын тайлан', 'iltodbaidal/sanhuuaudit2023.pdf'],
     ['2022 оны санхүүгийн тайланд хийсэн аудитын тайлан', 'iltodbaidal/audit2022.pdf'],
     ['2018 оны санхүүгийн тайлан өгсөн хөндлөнгийн аудитын дүгнэлт', 'iltodbaidal/audit2018.pdf'],
