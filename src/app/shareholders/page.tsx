f94cf982a8ee991fa02b683442dee9aab009b1a4
Gundalai <gundalai@mtcone.net>
2026-04-27 22:33:56 +0800

    a

--- src/app/shareholders/page.tsx
+++ src/app/shareholders/page.tsx
@@ -44,7 +44,7 @@ const Page = () => {
               </button>
             </Link> */}
               <a
-                href="/assets/huwtsaa/hewleliin-eh.pdf"
+                href="/assets/huwtsaa/he.pdf"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
