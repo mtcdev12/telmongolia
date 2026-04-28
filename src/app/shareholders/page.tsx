3ef43a75f6a0eab79e7ae7db2e039a9c26694f94
Gundalai <gundalai@mtcone.net>
2026-04-28 12:43:49 +0800

    zaswar

--- src/app/shareholders/page.tsx
+++ src/app/shareholders/page.tsx
@@ -52,7 +52,7 @@ const Page = () => {
                 Хурлын материал нээх
               </a>
               <a
-                href="/assets/huwtsaa/sanaliin-huudas.pdf"
+                href="/assets/huwtsaa/sanal.pdf"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500"
