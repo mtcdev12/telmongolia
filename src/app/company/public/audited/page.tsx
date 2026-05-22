f978735ee4d3e0f239e47f3eeb7d5786ee788b55
Gundalai <gundalai@mtcone.net>
2026-05-22 13:48:38 +0800

    2025

--- src/app/company/public/audited/page.tsx
+++ src/app/company/public/audited/page.tsx
@@ -3,11 +3,12 @@ import PdfTable from "../../pdfTable";
 
 const breadcrumb = ["Ил тод байдал", "Аудитлагдсан тайлан"];
 const duties = [
+    ['2025 оны санхүүгийн аудитын тайлан', 'iltodbaidal/1. Profile 1.2. Audited balance 2026.pdf'],
     ['2024 оны санхүүгийн аудитын тайлан', 'iltodbaidal/audited balance 2025.pdf'],
     ['2023 оны санхүүгийн аудитын тайлан', 'iltodbaidal/sanhuuaudit2023.pdf'],
     ['2022 оны санхүүгийн тайланд хийсэн аудитын тайлан', 'iltodbaidal/audit2022.pdf'],
     ['2018 оны санхүүгийн тайлан өгсөн хөндлөнгийн аудитын дүгнэлт', 'iltodbaidal/audit2018.pdf'],
-    ['2017 оны санхүүгийн тайлан өгсөн хөндлөнгийн аудитын дүгнэлт', 'iltodbaidal/audit2017.pdf']
+    ['2017 оны санхүүгийн тайлан өгсөн хөндлөнгийн аудитын дүгнэлт', 'iltodbaidal/audit2017.pdf'],
     
 ];
 const Page = () => {
